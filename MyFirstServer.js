9263f75c31d0d9683beab6bf8f20526cd22bcdc3
shaypiram <88582206+shaypiram@users.noreply.github.com>
2021-09-18 16:31:45 +0300

    Add files via upload

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,5 +1,34 @@
-var http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const sql = require("./db.js");
 
-http.createServer(function (request, response) {
-    response.end('Hello World');
-}).listen(8080);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+//route
+app.get("/", (req, res) => {
+	res.json({ message: "welcome" });
+});
+
+//another route - what happens when we arrived to localhost:8080/customers
+app.get("/customers", function (req, res) {
+	sql.query("SELECT * FROM customers", (err, mysqlres) => {
+		if (err) {
+			console.log("error: ", err);
+			res.status(400).send({ message: "error in getting all customers: " + err });
+			return;
+		}
+		console.log("got all customers...");
+		res.send(mysqlres);
+		return;
+	});
+});
+
+app.listen(8080, () => {
+	console.log("Server is running on port 8080");
+});
+
